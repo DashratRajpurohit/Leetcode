12d1f77c410b0cef8e5bafcb4441e59383cba785
Dashrat Singh Rajpurohit <dashrat.rajpurohit.cg@gmail.com>
2026-03-30 14:21:38 +0530

    Time: 44 ms (71.35%) | Memory: 57.1 MB (31.52%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -4,18 +4,9 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    let count=0;
     let arr1 = [];
-    let arr2 = [];
-    for(let i=0;i<arr.length;i++){
-        count++;
-        arr1.push(arr[i]);
-        if(count==size){
-            arr2.push(arr1);
-            arr1 = [];
-            count =0;
-        }  
+    for(let i=0; i< arr.length; i+=size){
+        arr1.push(arr.slice(i, i+size))
     }
-    if(arr1.length>=1)arr2.push(arr1);
-    return arr2;
+    return arr1;
 };
