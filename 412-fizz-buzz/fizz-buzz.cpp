51b0f48fdfa596b744f326be4b8c6f1dd74bfd8e
Dashrat Singh Rajpurohit <dashrat.rajpurohit.cg@gmail.com>
2026-04-09 11:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (17.52%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>arr;
+        for(int i=1;i<=n;i++){
+            if(i%3==0&&i%5==0){
+                arr.push_back("FizzBuzz");
+                continue;
+            }
+            else if(i%3==0){
+                 arr.push_back("Fizz");
+                 continue;
+            }
+            else if(i%5==0){
+                 arr.push_back("Buzz");
+                 continue;
+            }
+            else{
+                arr.push_back(to_string(i));
+            }
+        }
+        return arr;
+    }
+};
