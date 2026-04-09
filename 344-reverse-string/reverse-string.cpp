6590d44a16ddfe3cd4ef52e7c2dbc197e61c1fe9
Dashrat Singh Rajpurohit <dashrat.rajpurohit.cg@gmail.com>
2026-04-09 11:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (51.60%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -3,9 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int i=0,j=s.size()-1;
         while(j>i){
-            char c = s[i];
-            s[i]=s[j];
-            s[j]=c;
+            swap(s[i],s[j]);
             i++;
             j--;
         }
