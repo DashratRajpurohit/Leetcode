5f42d90fc8ee934ab352d7c7d4fe22fa910eff63
Dashrat Singh Rajpurohit <dashrat.rajpurohit.cg@gmail.com>
2026-04-30 13:52:35 +0530

    Time: 3 ms (9.95%) | Memory: 9 MB (34.93%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        sort(sentence.begin(),sentence.end());
-        int count=0;
-        for(int i=0;i<sentence.size()-1;i++){
-            if(sentence[i]!=sentence[i+1])count++;
-        }
-        return (count+1==26);
+    return unordered_set<char>(sentence.begin(),sentence.end()).size()==26;
     }
 };
