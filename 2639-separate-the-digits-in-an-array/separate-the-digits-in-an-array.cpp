d3685095b3102a4c7077fc96387336817791a75a
Dashrat Singh Rajpurohit <dashrat.rajpurohit.cg@gmail.com>
2026-06-25 18:08:02 +0530

    Time: 19 ms (5.38%) | Memory: 14.1 MB (87.34%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    vector<int>arr;
-    int check(int c){
-        while(c>0){
-            arr.insert(arr.begin(), c%10);
-            c/=10;
-        }
-        return c;
-    }
     vector<int> separateDigits(vector<int>& nums) {
-        
+        vector<int>arr;    
         for(int i=nums.size()-1;i>=0;i--){
             if(nums[i]>9){
-                check(nums[i]);
+                while(nums[i]>0){
+                    arr.insert(arr.begin(), nums[i]%10);
+                    nums[i]/=10;
+                }
             }
             else arr.insert(arr.begin(), nums[i]);
         }
